a21e6f30d57e890969e84bdd941a50b335b91f0d
пуауа <your_email@whatever.com>
2025-09-29 14:56:40 +0500

    practice-3

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from apps.task.views import welcom_page,list_user,cities,cities_time,Counter,inc_counter,reset_counter
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',view=welcom_page,name="welcom_page"),
+    path('users/',view=list_user),
+    path('cities/',view=cities),
+    path('city_time/',view = cities_time),
+    path('cnt/',view = Counter, name = "counter"),
+    path('cnt/inc_counter/',view = inc_counter,name = "inc_counter"),
+    path('cnt/reset_counter/',view = reset_counter, name = "reset_counter")
 ]
