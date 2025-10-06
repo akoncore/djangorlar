f189d5c7d1b472ebcc8a4d6fdc41ed3699d8ee82
пуауа <your_email@whatever.com>
2025-10-06 12:42:08 +0500

    practice-4-models

--- apps/models/apps.py
+++ apps/models/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class ModelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.task'
+    name = 'apps.models'
+
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.task'
+    'apps.task',
+    'apps.models'
 ]
 
 MIDDLEWARE = [
